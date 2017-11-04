jonchang/homebrew-core-sharded

commit c2642067c8d8e6ae2319708b6fd94ba5560a3683

wine: bump dependencies
Closes #13841.

Signed-off-by: ilovezfs <ilovezfs@icloud.com>

Formula/w/wine.rb
@@ -5,6 +5,7 @@
 class Wine < Formula
   desc "Run Windows applications without a copy of Microsoft Windows"
   homepage "https://www.winehq.org/"
+  revision 1
   head "https://source.winehq.org/git/wine.git"
 
   stable do
@@ -69,8 +70,8 @@ class Wine < Formula
   end
 
   resource "mono" do
-    url "https://dl.winehq.org/wine/wine-mono/4.6.4/wine-mono-4.6.4.msi", :using => :nounzip
-    sha256 "91b7d58177b9a9355edf007dab94535471aebdddae12734ceb4a219d2ecc4152"
+    url "https://dl.winehq.org/wine/wine-mono/4.7.0/wine-mono-4.7.0.msi", :using => :nounzip
+    sha256 "7698474dd9cb9eb80796b5812dff37386ba97b78b21ca23b20079ca5ad6ca5a1"
   end
 
   resource "openssl" do
@@ -92,9 +93,9 @@ class Wine < Formula
   end
 
   resource "libtiff" do
-    url "http://download.osgeo.org/libtiff/tiff-4.0.7.tar.gz"
-    mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/t/tiff/tiff_4.0.7.orig.tar.gz"
-    sha256 "9f43a2cfb9589e5cecaa66e16bf87f814c945f22df7ba600d63aac4632c4f019"
+    url "http://download.osgeo.org/libtiff/tiff-4.0.8.tar.gz"
+    mirror "https://fossies.org/linux/misc/tiff-4.0.8.tar.gz"
+    sha256 "59d7a5a8ccd92059913f246877db95a2918e6c04fb9d43fd74e5c3390dac2910"
   end
 
   resource "little-cms2" do
@@ -110,9 +111,9 @@ class Wine < Formula
   end
 
   resource "freetype" do
-    url "https://downloads.sourceforge.net/project/freetype/freetype2/2.7.1/freetype-2.7.1.tar.bz2"
-    mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.7.1.tar.bz2"
-    sha256 "3a3bb2c4e15ffb433f2032f50a5b5a92558206822e22bfe8cbe339af4aa82f88"
+    url "https://downloads.sourceforge.net/project/freetype/freetype2/2.8/freetype-2.8.tar.bz2"
+    mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.8.tar.bz2"
+    sha256 "a3c603ed84c3c2495f9c9331fe6bba3bb0ee65e06ec331e0a0fb52158291b40b"
   end
 
   resource "libusb" do
@@ -144,9 +145,9 @@ class Wine < Formula
   end
 
   resource "libgphoto2" do
-    url "https://downloads.sourceforge.net/project/gphoto/libgphoto/2.5.12/libgphoto2-2.5.12.tar.bz2"
-    mirror "https://fossies.org/linux/privat/libgphoto2-2.5.12.tar.bz2"
-    sha256 "b9bb28990fde45ac385e4851a07dbad2e1250404b535b0a3a3b898bb431e4e2e"
+    url "https://downloads.sourceforge.net/project/gphoto/libgphoto/2.5.13/libgphoto2-2.5.13.tar.bz2"
+    mirror "https://fossies.org/linux/privat/libgphoto2-2.5.13.tar.bz2"
+    sha256 "ceaacbdf187d1cd1aed5336991f46b0100f6960b6c8383f9aeab98f1f64780ef"
   end
 
   resource "net-snmp" do
@@ -155,16 +156,10 @@ class Wine < Formula
   end
 
   resource "sane-backends" do
-    url "https://mirrors.kernel.org/debian/pool/main/s/sane-backends/sane-backends_1.0.25.orig.tar.gz"
-    mirror "https://fossies.org/linux/misc/sane-backends-1.0.25.tar.gz"
-    sha256 "a4d7ba8d62b2dea702ce76be85699940992daf3f44823ddc128812da33dc6e2c"
-  end
-
-  # Fixes some missing headers missing error. Reported upstream
-  # https://lists.alioth.debian.org/pipermail/sane-devel/2015-October/033972.html
-  resource "sane-backends-patch" do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/6dd7790c/sane-backends/1.0.25-missing-types.patch"
-    sha256 "f1cda7914e95df80b7c2c5f796e5db43896f90a0a9679fbc6c1460af66bdbb93"
+    url "https://alioth.debian.org/frs/download.php/file/4224/sane-backends-1.0.27.tar.gz"
+    mirror "https://mirrors.kernel.org/debian/pool/main/s/sane-backends/sane-backends_1.0.27.orig.tar.gz"
+    mirror "https://fossies.org/linux/misc/sane-backends-1.0.27.tar.gz"
+    sha256 "293747bf37275c424ebb2c833f8588601a60b2f9653945d5a3194875355e36c9"
   end
 
   resource "mpg123" do
@@ -427,11 +422,6 @@ class Wine < Formula
 
       resource("sane-backends").stage do
         save_env do
-          # Cannot have "patch do" here
-          Pathname.pwd.install resource("sane-backends-patch")
-          system "patch", "-p1", "-i", "1.0.25-missing-types.patch"
-
-          ENV.deparallelize
           system "./configure", "--disable-dependency-tracking",
                                 "--prefix=#{libexec}",
                                 "--localstatedir=#{var}",
@@ -440,7 +430,6 @@ class Wine < Formula
                                 "--enable-libusb",
                                 "--disable-latex",
                                 *depflags
-          system "make"
           system "make", "install"
         end
       end

commit 65e5e427388126c31d11649028b9a2f050b02a45

wine: match bundled mono with the version expected by stable
Expected versions of Mono and Gecko are hardwired into the Wine source code:
  https://source.winehq.org/git/wine.git/blob/HEAD:/dlls/appwiz.cpl/addons.c

If these exact versions are not found by the initialization process
(e.g. wineboot), the process will fail. This failure takes several
minutes on headless systems (e.g. Travis CI):
  https://travis-ci.org/vszakats/harbour-core/builds/296594856#L2117-L2122

Matching addon versions for current wine 2.0.3 can be looked up here:
  https://source.winehq.org/git/wine.git/blob/refs/tags/wine-2.0.3:/dlls/appwiz.cpl/addons.c

Regression from: c2642067c8d8e6ae2319708b6fd94ba5560a3683

* Also extend comment to make sure that the hardwired addon versions are
  matched with the bundled (Mono and Gecko) resources.
* Also bump OpenSSL to its latest revision

Closes #20232.

Signed-off-by: Viktor Szakats <vszakats@users.noreply.github.com>

Formula/w/wine.rb
@@ -1,11 +1,13 @@
-# NOTE: When updating Wine, please check Wine-Gecko and Wine-Mono for updates
-# too:
+# NOTE: When updating Wine, please make sure to match Wine-Gecko and Wine-Mono
+# versions:
 #  - https://wiki.winehq.org/Gecko
 #  - https://wiki.winehq.org/Mono
+# with `GECKO_VERSION` and `MONO_VERSION`, as in:
+#    https://source.winehq.org/git/wine.git/blob/refs/tags/wine-2.0.3:/dlls/appwiz.cpl/addons.c
 class Wine < Formula
   desc "Run Windows applications without a copy of Microsoft Windows"
   homepage "https://www.winehq.org/"
-  head "https://source.winehq.org/git/wine.git"
+  revision 1
 
   stable do
     url "https://dl.winehq.org/wine/source/2.0/wine-2.0.3.tar.xz"
@@ -25,6 +27,11 @@ class Wine < Formula
       url "https://bugs.winehq.org/attachment.cgi?id=55968"
       sha256 "1b5086798ce6dc959b3cbb8f343ee236ae06c7910e4bbae7d9fde3f162f03a79"
     end
+
+    resource "mono" do
+      url "https://dl.winehq.org/wine/wine-mono/4.6.4/wine-mono-4.6.4.msi", :using => :nounzip
+      sha256 "91b7d58177b9a9355edf007dab94535471aebdddae12734ceb4a219d2ecc4152"
+    end
   end
 
   bottle do
@@ -43,6 +50,20 @@ class Wine < Formula
     patch do
       url "https://raw.githubusercontent.com/Homebrew/formula-patches/74c2566/wine/2.14.patch"
       sha256 "6907471d18996ada60cc0cbc8462a1698e90720c0882846dfbfb163e5d3899b8"
+    end
+
+    resource "mono" do
+      url "https://dl.winehq.org/wine/wine-mono/4.7.1/wine-mono-4.7.1.msi", :using => :nounzip
+      sha256 "2c8d5db7f833c3413b2519991f5af1f433d59a927564ec6f38a3f1f8b2c629aa"
+    end
+  end
+
+  head do
+    url "https://source.winehq.org/git/wine.git"
+
+    resource "mono" do
+      url "https://dl.winehq.org/wine/wine-mono/4.7.1/wine-mono-4.7.1.msi", :using => :nounzip
+      sha256 "2c8d5db7f833c3413b2519991f5af1f433d59a927564ec6f38a3f1f8b2c629aa"
     end
   end
 
@@ -68,15 +89,10 @@ class Wine < Formula
     sha256 "c565ea25e50ea953937d4ab01299e4306da4a556946327d253ea9b28357e4a7d"
   end
 
-  resource "mono" do
-    url "https://dl.winehq.org/wine/wine-mono/4.7.1/wine-mono-4.7.1.msi", :using => :nounzip
-    sha256 "2c8d5db7f833c3413b2519991f5af1f433d59a927564ec6f38a3f1f8b2c629aa"
-  end
-
   resource "openssl" do
-    url "https://www.openssl.org/source/openssl-1.0.2l.tar.gz"
-    mirror "https://dl.bintray.com/homebrew/mirror/openssl-1.0.2l.tar.gz"
-    sha256 "ce07195b659e75f4e1db43552860070061f156a98bb37b672b101ba6e3ddf30c"
+    url "https://www.openssl.org/source/openssl-1.0.2m.tar.gz"
+    mirror "https://dl.bintray.com/homebrew/mirror/openssl-1.0.2m.tar.gz"
+    sha256 "8c6ff15ec6b319b50788f42c7abc2890c08ba5a1cdcd3810eb9092deada37b0f"
   end
 
   resource "libtool" do